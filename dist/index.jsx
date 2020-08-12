c2312d58c7a703c22748fe325dbe8b6b4fed8213
Tim Haywood <tim@haywood.org>
2020-08-12 18:43:46 +1000

    commit dist to git

---
+++ dist/index.jsx
@@ -0,0 +1,16 @@
+{
+	"defaultFunction": function() {
+	    return 'Default Function Value';
+	},
+	
+	"anotherFunction": function() {
+	    return 'Another Function';
+	},
+	
+	// Import values from other files
+	// Or define values in index
+	"localFunction": function() {
+	    return 'Local Function';
+	},
+	"localValue":  2,
+}
